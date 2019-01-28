6b6fe8f21e944e9f29b968d8e4c54c7976cbd8a6
J0KER <dionatsir@gmail.com>
2019-01-28 11:17:21 +0700

    orderby date event

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -14,7 +14,7 @@ class EventController extends Controller
      */
     public function index()
     {
-        $event = Event::all();
+        $event = Event::orderBy('date','desc')->get();
         return view('event', compact('event'));
     }
 
